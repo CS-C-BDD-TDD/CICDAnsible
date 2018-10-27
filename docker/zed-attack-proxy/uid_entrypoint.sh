26265e45941a0f1afb0cd79f314e644de173b00e
Deven Phillips <deven.phillips@gmail.com>
2018-10-27 11:37:02 -0400

    Working Dockerfile implementation

---
+++ docker/zed-attack-proxy/uid_entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
+zap-x.sh -daemon -host 0.0.0.0 -port -8080 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true
