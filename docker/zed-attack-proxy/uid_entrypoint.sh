3999e295882c6fee453e673074880a4bc6faabf5
Deven Phillips <deven.phillips@gmail.com>
2018-10-27 13:37:38 -0400

    Added debug log statement

--- docker/zed-attack-proxy/uid_entrypoint.sh
+++ docker/zed-attack-proxy/uid_entrypoint.sh
@@ -2,6 +2,7 @@
 
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
+    echo "Storing container UID in /etc/passwd to support ZAP"
     echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
   fi
 fi
