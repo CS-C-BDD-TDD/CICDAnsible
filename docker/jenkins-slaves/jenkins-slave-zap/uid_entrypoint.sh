ecac83d343a742e68b1fe3d00dd741c1296b0b7e
Deven Phillips <deven.phillips@gmail.com>
2018-10-28 10:25:31 -0400

    Working toward working ZAP configuration

---
+++ docker/jenkins-slaves/jenkins-slave-zap/uid_entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "Storing container UID in /etc/passwd to support ZAP"
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
+# Execute the provided command with arguments AFTER the UID setup
+# Run the process in the background and output the PID
+$@ &
+echo $!
